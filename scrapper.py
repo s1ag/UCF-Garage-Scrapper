269e5987147771016c929038062ad59de5e4906a
s1ag <s1ag@protonmail.com>
2019-12-23 14:09:59 -0500

    Created first set of helper functions, formatted the script functionally

--- scrapper.py
+++ scrapper.py
@@ -1,46 +1,58 @@
 import requests
 from bs4 import BeautifulSoup
 
+# Global in case URI changes 
 url = 'https://secure.parking.ucf.edu/GarageCount/iframe.aspx'
-page = requests.get(url)
 
-soup = BeautifulSoup(page.content, 'html.parser')
+def getGarageSite():
+    """Helper function that requests and returns the UCF Parking Garage website"""
 
-garages = []
-current_spots = []
-max_spots = []
-
-# table where the data is gonna come from
-table = soup.find('table', id='gvCounts_DXMainTable')
-
-# get max spots
-for length in soup.find_all("strong"):
-    x = length.next_sibling.strip()
-    x = x.strip('/')
-    max_spots.append(int(x)) 
-
-# get current spots
-for length in soup.find_all("strong"):
-    x = int (length.get_text())
-    current_spots.append(x)
-
-for length in soup.find_all('td',{'class':'dxgv'}, text=True):
-    x = length.get_text()
-    garages.append(x)
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    return soup
 
 
-print(max_spots)
-print(current_spots)
-    # for each cell in each row
-print(f"{garages[0]}")
-print(f"{current_spots[0]} / {max_spots[0]}:" )
-print(f"{current_spots[0] / max_spots[0] * 100:.2f} is percentage")
-print(f"{100 - (current_spots[0] / max_spots[0] * 100):.2f} is full")
+def getMaxSpots(soup):
+    """Returns array containing the max spots for every parking garage"""
+    max_spots = []
+    for length in soup.find_all("strong"):
+        x = length.next_sibling.strip()
+        x = x.strip('/')
+        max_spots.append(int(x))
+    return max_spots 
 
 
-# percentage = (int(current_spots[0]) / int(max_spots[0])) * 100
-# print(f"Percentage : {percentage}")
-# print(f"Total percentage {current_spots[0]/max_spots[0]}")
+def getSpotsLeft(soup):
+    """Returns array containing the total spots left for every parking garage"""
+    current_spots = []
+    for length in soup.find_all("strong"):
+        x = int (length.get_text())
+        current_spots.append(x)
+    return current_spots
 
-# function to scrape website and return parking lot arrays
-# def getParking():
+def getGarageNames(soup):
+    """Returns array containing the name of each garage field"""
+    garageNames = []
+    for length in soup.find_all('td',{'class':'dxgv'}, text=True):
+        x = length.get_text()
+        garageNames.append(x)
+    return  garageNames
+
+
+def garageStats():
+    """Prints a table to STDOUT containing all the parking garages and their respective data"""
+
+    soup = getGarageSite()
+    maxSpots = getMaxSpots(soup)
+    spotsLeft = getSpotsLeft(soup)
+    names = getGarageNames(soup)
+    for i in range(len(names)):
+        print(f"{names[i]}    : {spotsLeft[i]} / {maxSpots[i]} | {maxSpots[i] - spotsLeft[i]} Spots Taken ~ {100 - (spotsLeft[i]/maxSpots[i] * 100): .0f}% Full")
+
+
+def main():
+    garageStats()
+
+
+if __name__ == "__main__":
+    main()
